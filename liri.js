d1c63be89196362c6fae2ca29ae412b30b243339
Mary <marykwong27@gmail.com>
2019-04-30 12:48:35 -0700

    working on js file to add functions

--- liri.js
+++ liri.js
@@ -1,3 +1,44 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+//fs library
+var fs = require("fs");
+
+//variables for the input
+var whichAPI = process.argv[2];
+var name = process.argv[3];
+
+//switch statement depending on first input 
+
+switch(whichAPI) {
+    case "spotify-this-song":
+    spot(name);
+    break;
+
+    case "concert-this":
+    bandsInTown(name);
+    break;
+    
+    case "movie-this":
+    movie(name);
+    break;
+
+    case "do-what-it-says":
+    doWhat(name);
+    break;
+}
+
+//spotify function 
+
+function spot(name) {
+    //no song picked
+    if(!name)
+    {
+        name = "The Sign";
+    }
+    else{
+        
+    }
+
+}
